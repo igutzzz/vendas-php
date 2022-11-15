f8a87d442bf27435580c8c059f9e1fbeb2bb7004
Igor de Almeida <ofc.iguituu@gmail.com>
2022-11-15 17:58:13 -0300

    Início do desenvolvimento

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Vendas</title>
+    <link rel="stylesheet" href="./styles/index.css">
+</head>
+<body>
+    <nav>
+        <h1>Vendas</h1>
+    </nav>
+    <main>
+        <h2>Menu</h2>
+        <div id="menu">
+            <button>Cadastro de Produtos</button>
+            <button>Lista de Produtos</button>
+            <button>Cadastro de Clientes</button>
+            <button>Lista de Clientes</button>
+            <button>Nova Venda</button>
+            <button>Listar Vendas (Cliente)</button>
+            <button>Imprimir Pedido</button>
+        </div>
+    </main>
+</body>
+</html>
---
+++ pages/produto-cadastro.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Vendas</title>
+    <link rel="stylesheet" href="./styles/index.css">
+</head>
+<body>
+    <nav>
+        <h1>Vendas</h1>
+    </nav>
+    
+</body>
+</html>
